077f95687c434d96cff9cb633869ccd1ce53d8ce
John Doe <johndoe@example.com>
2020-10-09 17:34:32 -0500

    page 229

--- alien_invasion.py
+++ alien_invasion.py
@@ -2,3 +2,14 @@ import sys
 
 import pygame
 
+class AlienInvasion: #overall class to manage game assets and behavior. Empty window. 
+    def __init__(self): #initialize game and create resources 
+        pygame.init()
+
+        self.screen = pygame.display.set_mode((1200,800))
+        pygame.display.set_caption("Alien Invasion")
+
+    def run_game(self): #start the main loop for the game
+        while True: #watch for keyboard and mouse events
+            for event in pygame.event.get():
+                
